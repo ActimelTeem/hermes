3b97ab2a39b73bcf918d3870af76d02390f9b0e0
Moleque <molecada@yandex.ru>
2017-12-14 18:14:23 +0300

    Рабочая версия бота. Необходимо доделать ввод пароля и вынести переменные в отдельный файл.

--- hermes.py
+++ hermes.py
@@ -1,17 +1,16 @@
 """
 + выводить информацию в консоль о подключении и т.д.
 пароль не засвечивать
-подтверждение от курьера по заказу
++ подтверждение от курьера по заказу
 + геолокация
+вынести в файл токен, ключ гугла
 
 from hermes.constants import token
 """
 
-
 import telebot
 import sqlite3
 import requests
-#import string
 
 Token = '467379466:AAFfwviUU9AHO2Sb9bs8YGssFYGmE9EhbB4'
 bot = telebot.TeleBot(Token)
@@ -27,6 +26,15 @@ class DataBase: #класс для работы с БД
     def login(self, log, pw):   #проверка пользовательских данных в БД при авторизации
         self.cursor.execute("SELECT * FROM crm_couriers WHERE login = :login AND password = :password", {"login": log, "password": pw})
         return self.cursor.fetchone()
+
+    def check_order(self, courier_id):  #проверка на наличие у данного курьера заказа в БД
+        self.cursor.execute("SELECT * FROM crm_order WHERE order_status_id = 3 AND order_courier_id = :courier_id", {"courier_id": courier_id})
+        answer = self.cursor.fetchone()
+        try:
+            answer[0]
+            return answer
+        except TypeError:
+            return False
 
     def get_order(self, courier_id):    #получение информации о первом доступном заказе из БД
         self.cursor.execute("SELECT * FROM crm_order WHERE order_status_id = 4")
@@ -66,12 +74,26 @@ class Order:
         self.manager = result[9]  # order_manager id,
         self.courier = result[10]  # order_courier,
 
-    def order_info(self):   #получение строки с информацией о заказе
+    def info(self):   #получение строки с информацией о заказе
         info = "Номер: {}\nСодержимое: {}\nАдрес: {}\nКлиент: {}\nКоментарий: {}\nСтоимость: {}\nДата оформления: {}\nПлатежный статус: {}\n".format(self.number, self.content, self.location, self.client, self.client_comment, self.cost, self.date, self.payment_status)
         key = "AIzaSyCV-7aHiRjSshta2PHDrQ8aeemiWBzHfgc"
         url = "https://maps.googleapis.com/maps/api/geocode/json?address={}&key={}".format(self.location.replace(" ", "+"), key)
         results = requests.get(url).json()
-        return info, results["results"][0]["geometry"]["location"]
+        try:
+            return info, results["results"][0]["geometry"]["location"]
+        except IndexError:
+            return info, False
+
+    def complete(self):
+        DB = DataBase("db.sqlite3")
+        DB.change_order_status(self.number, 2)  # изменение статуса взятого заказа на выполненный
+        DB.close()
+
+    def cancel(self, text):
+        DB = DataBase("db.sqlite3")
+        DB.courier_comment(self.number, text)
+        DB.change_order_status(self.number, 1)  # изменение статуса взятого заказа на отмененный
+        DB.close()
 
 class Courier:  #класс курьера
     def __init__(self): #инициализация нового курьера
@@ -98,25 +120,31 @@ class Courier:  #класс курьера
         except:
             return False
 
-    def new_order(self):    #получение нового заказа
+    def is_order(self):
         DB = DataBase("db.sqlite3")
-        result = DB.get_order(self.id)
+        result = DB.check_order(self.id)
         if result != False:
             self.Ord = Order(result)
-            DB.change_order_status(self.Ord.number, 3) #изменение статуса взятого заказа на выполняющийся
-            DB.close()
-            self.state = "have_order"
+            self.state = "order"
             self.order_status = True
             return True
         else:
-            DB.close()
             return False
 
-    def cancel_order(self, text):
-        DB = DataBase("db.sqlite3")
-        DB.courier_comment(self.Ord.number, text)
-        DB.change_order_status(self.Ord.number, 1)  # изменение статуса взятого заказа на выполняющийся
-        DB.close()
+    def new_order(self):    #получение нового заказа
+        if not self.is_order():
+            DB = DataBase("db.sqlite3")
+            result = DB.get_order(self.id)
+            if result != False:
+                self.Ord = Order(result)
+                DB.change_order_status(self.Ord.number, 3) #изменение статуса взятого заказа на выполняющийся
+                DB.close()
+                self.state = "order"
+                self.order_status = True
+                return True
+            else:
+                DB.close()
+                return False
 
 
 #декораторы
@@ -143,7 +171,9 @@ def login(message):    #/login
 @bot.message_handler(commands=['exit'])
 def logout(message):    #/logout
     if message.chat.id in courier_list and courier_list[message.chat.id].login_status == True:
-        bot.send_message(message.chat.id, "{} вышел из системы".format(courier_list[message.chat.id].login))
+        keyboard = telebot.types.ReplyKeyboardMarkup()
+        keyboard.row("Авторизация")
+        bot.send_message(message.chat.id, "{} вышел из системы".format(courier_list[message.chat.id].login), reply_markup=keyboard)
         print("{} вышел из системы.".format(courier_list[message.chat.id].login))
         courier_list.pop(message.chat.id)
     else:
@@ -154,20 +184,36 @@ def logout(message):    #/logout
 def get_new_order(message):
     if courier_list[message.chat.id].order_status == True:
         bot.send_message(message.chat.id, "Уже есть активный заказ:")
-        info = courier_list[message.chat.id].Ord.order_info()
+        info = courier_list[message.chat.id].Ord.info()
         bot.send_message(message.chat.id, info)
     else:
         if courier_list[message.chat.id].new_order():
             keyboard = telebot.types.ReplyKeyboardMarkup()
             keyboard.row("Подтверждение", "Отмена")
             bot.send_message(message.chat.id, "Был получен заказ:", reply_markup=keyboard)
-            info, locations = courier_list[message.chat.id].Ord.order_info()
+            info, locations = courier_list[message.chat.id].Ord.info()
             bot.send_message(message.chat.id, info)
-            bot.send_location(message.chat.id, locations["lat"], locations["lng"])
+            if locations != False:
+                bot.send_location(message.chat.id, locations["lat"], locations["lng"])
             courier_list[message.chat.id].state = "request"
             print("{} получил заказ №{}.".format(courier_list[message.chat.id].login, courier_list[message.chat.id].Ord.number))
         else:
             bot.send_message(message.chat.id, "Нет доступных заказов")
+
+@bot.message_handler(commands=['complete'])
+@is_login
+def complete_order(message):
+    if courier_list[message.chat.id].order_status == True:
+        courier_list[message.chat.id].Ord.complete()
+        courier_list[message.chat.id].order_status = False
+        courier_list[message.chat.id].state = "in_system"
+        keyboard = telebot.types.ReplyKeyboardMarkup()
+        keyboard.row("Получить заказ")
+        keyboard.row("Выход")
+        bot.send_message(message.chat.id, "Заказ №{} выполнен".format(courier_list[message.chat.id].Ord.number), reply_markup=keyboard)
+        print("{} выполнил заказ №{}.".format(courier_list[message.chat.id].login, courier_list[message.chat.id].Ord.number))
+    else:
+        bot.send_message(message.chat.id, "Нет активных заказов")
 
 @bot.message_handler(commands=['cancel'])
 @is_login
@@ -176,18 +222,29 @@ def cancel_order(message):
         print("{} отменил заказ №{}.".format(courier_list[message.chat.id].login, courier_list[message.chat.id].Ord.number))
         bot.send_message(message.chat.id, "Напишите причину отказа:")
         courier_list[message.chat.id].state = "cancel"
+    else:
+        bot.send_message(message.chat.id, "Нет активного заказа")
 
 @bot.message_handler(content_types = ['text'])
 def check_answer(message):
     if message.chat.id not in courier_list or courier_list[message.chat.id].state == "no":
-        bot.send_message(message.chat.id, "Для использования сервиса войдите в систему - /start")
+        if message.text == "Авторизация":
+            login(message)
+        else:
+            bot.send_message(message.chat.id, "Для использования сервиса войдите в систему - /start")
 
     elif courier_list[message.chat.id].state == "password":
         courier_list[message.chat.id].password = message.text
         if courier_list[message.chat.id].login_system():
             keyboard = telebot.types.ReplyKeyboardMarkup()
-            keyboard.row("Получить заказ")
-            keyboard.row("Выход")
+            if courier_list[message.chat.id].is_order():
+                keyboard.row("Информация по заказу")
+                keyboard.row("Заказ выполнен")
+                keyboard.row("Отменить заказ")
+                keyboard.row("Выход")
+            else:
+                keyboard.row("Получить заказ")
+                keyboard.row("Выход")
             bot.send_message(message.chat.id, "Вы были авторизованы как {}\n({} {})".format(courier_list[message.chat.id].login, courier_list[message.chat.id].name, courier_list[message.chat.id].surname), reply_markup=keyboard)
             print("{} был авторизован ({} {}).".format(courier_list[message.chat.id].login, courier_list[message.chat.id].name, courier_list[message.chat.id].surname))
         else:
@@ -202,28 +259,45 @@ def check_answer(message):
     elif courier_list[message.chat.id].login_status:
         if message.text == "Выход":
             logout(message)
-        if message.text == "Получить заказ":
-            get_new_order(message)
 
-    elif courier_list[message.chat.id].state == "request":
-        if message.text == "Подтверждение":
+        elif courier_list[message.chat.id].state == "request":
+            if message.text == "Подтверждение":
+                keyboard = telebot.types.ReplyKeyboardMarkup()
+                keyboard.row("Информация по заказу")
+                keyboard.row("Заказ выполнен")
+                keyboard.row("Отменить заказ")
+                keyboard.row("Выход")
+                bot.send_message(message.chat.id, "Заказ подтвержден", reply_markup=keyboard)
+                courier_list[message.chat.id].state = "order"
+                print("{} подтвердил получение заказа №{}.".format(courier_list[message.chat.id].login,
+                                                                   courier_list[message.chat.id].Ord.number))
+            elif message.text == "Отмена":
+                cancel_order(message)
+            else:
+                bot.send_message(message.chat.id, "Необходимо подтвердить заказ")
+
+        elif courier_list[message.chat.id].state == "cancel":
+            courier_list[message.chat.id].Ord.cancel(message.text)
+            courier_list[message.chat.id].state = "in_system"
+            courier_list[message.chat.id].order_status = False
             keyboard = telebot.types.ReplyKeyboardMarkup()
-            keyboard.row("Информация по заказу")
-            keyboard.row("Заказ выполнен")
-            keyboard.row("Отменить заказ")
+            keyboard.row("Получить заказ")
             keyboard.row("Выход")
-            bot.send_message(message.chat.id, reply_markup=keyboard)
-            courier_list[message.chat.id].state = "order"
-            print("{} подтвердил получение заказа №{}.".format(courier_list[message.chat.id].login, courier_list[message.chat.id].Ord.number))
-        elif message.text == "Отмена":
-            cancel_order(message)
-        else:
-            bot.send_message(message.chat.id, "Необходимо подтвердить заказ")
+            bot.send_message(message.chat.id, "Заказ отменен", reply_markup=keyboard)
 
-    elif courier_list[message.chat.id].state == "cancel":
-        courier_list[message.chat.id].cancel(message.text)
-        courier_list[message.chat.id].state = "in_system"
-        bot.send_message(message.chat.id, "Заказ отменен")
+        elif courier_list[message.chat.id].state == "order":
+            if message.text == "Информация по заказу":
+                info, locations = courier_list[message.chat.id].Ord.info()
+                bot.send_message(message.chat.id, info)
+                if locations != False:
+                    bot.send_location(message.chat.id, locations["lat"], locations["lng"])
+            elif message.text == "Заказ выполнен":
+                complete_order(message)
+            elif message.text == "Отменить заказ":
+                cancel_order(message)
+        else:
+            if message.text == "Получить заказ":
+                get_new_order(message)
 
 if __name__ == "__main__":
     bot.polling(none_stop = True, interval = 0)
